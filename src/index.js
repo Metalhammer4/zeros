0e23a074fe424933ea1324ef91646e3da1c0795a
Eric <ericmetalhammer@gmail.com>
2018-09-15 22:53:33 +0300

    Task 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+count = 0
+for (i = 5; i <= number; i *= 5)
+{
+count += Math.floor(number / i) 
+if (Math.floor(number / i)  == 0) break
+}
+ return count
 }
